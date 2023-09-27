b471adaaa608dfc16bd14ece917f773ca7bb06b0
VolumeElement <92828348+VolumeElement@users.noreply.github.com>
2023-09-27 17:18:44 -0400

    File Hashing

--- semantic_pdf/cli.py
+++ semantic_pdf/cli.py
@@ -1,4 +1,6 @@
-import search
+from hashing import hash_files
 from search import search_pdf_files
 
-print(search_pdf_files())
+file_list = search_pdf_files()
+file_hashes = hash_files(file_list)
+print(file_hashes)
--- semantic_pdf/hashing.py
+++ semantic_pdf/hashing.py
@@ -1,1 +1,21 @@
-from utils import config
+import hashlib
+
+
+def hash_file(file_path):
+    BUF_SIZE = 65536
+    md5 = hashlib.md5()
+
+    with open(file_path, "rb") as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+    return md5.hexdigest()
+
+
+def hash_files(file_paths):
+    file_hashes = []
+    for file_path in file_paths:
+        file_hashes.append([file_path, hash_file(file_path)])
+    return file_hashes
